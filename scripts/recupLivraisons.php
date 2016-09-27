9c46fa4ea59042012ad2bfa1c425ee657c732fd6
Valerian <Aerys59@gmail.com>
2016-09-27 11:45:25 +0200

    Fix conf in recupLivraisons.php

--- scripts/recupLivraisons.php
+++ scripts/recupLivraisons.php
@@ -4,11 +4,11 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
 
-  require_once("./conf.php");
+  require_once("conf.php");
 
   $conn = mysqli_connect($db_host,$db_user,$db_password, $db_db);
   $retour=$conn->query("SELECT Nom, Duree, Tarif FROM livraisons ");
-  
+
 
 
 
